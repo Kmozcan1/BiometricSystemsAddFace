8d1a453095cc19550484ff8463d30959577f3b8f
Kmozcan1 <kmozcan1@gmail.com>
2019-04-12 20:13:12 +0200

    Initial commit

---
+++ app/src/main/java/com/sapienza/cs/sapienzaaddface/Helpers/ConnectionHelper.java
@@ -0,0 +1,20 @@
+package com.sapienza.cs.sapienzaaddface.Helpers;
+
+import android.app.Application;
+
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.sapienza.cs.sapienzaaddface.R;
+
+public class ConnectionHelper extends Application {
+    private static FaceServiceClient faceServiceClient;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        faceServiceClient = new FaceServiceRestClient(getString(R.string.service_host), getString(R.string.subscription_key));
+    }
+
+    public static FaceServiceClient getFaceServiceClient() {
+        return faceServiceClient;
+    }
+}
